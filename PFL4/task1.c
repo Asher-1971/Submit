7927135e87acb3fc26f8c95550db65682116d6eb
Syed <127295261+Asher-1971@users.noreply.github.com>
2026-09-13 16:19:06 +0500

    lab4 taask1 done

--- PFL4/task1.c
+++ PFL4/task1.c
@@ -1,1 +1,11 @@
-nigga 123
+#include <stdio.h>
+
+int main(void){
+    int units = 650 ;
+    if (units > 500){
+        printf("High Usage");
+    }
+    else {
+        printf("Normal Usage");
+    }
+}
